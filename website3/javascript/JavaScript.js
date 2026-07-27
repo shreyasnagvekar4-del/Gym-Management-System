4a2c8b39f22ffb5cb90616868cf83737da72d3da
shreyasnagvekar4-del <shreyasnagvekar4@gmail.com>
2026-07-27 12:16:08 +0530

    Add files via upload

---
+++ website3/javascript/JavaScript.js
@@ -0,0 +1,146 @@
+﻿async function RegisterUser() {
+    let name =
+        document.getElementById("Name").value;
+    let email =
+        document.getElementById("Email").value;
+    let phone =
+        document.getElementById("Phone").value;
+    let password =
+        document.getElementById("Password").value;
+    let confirmpass =
+        document.getElementById("Confirm_Password").value;
+
+    if (password !== confirmpass) {
+        alert("Passwords do not match");
+        return false;
+    } 
+
+    let user = {
+        name : name,
+    email:email,
+    phone : phone,
+    passwordHash : password
+    };
+
+    let response = await fetch("https://localhost:7144/api/User",
+        {
+            method:"POST",
+            headers: {
+                "Content-Type":
+                    "application/json"
+            },
+            body:JSON.stringify(user)
+        }
+    );
+    
+    if (response.ok) {
+        let result = await response.json();
+        localStorage.setItem("loggedIn", "true");
+        localStorage.setItem("username", result.name);
+        localStorage.setItem("email", result.email);
+        alert("Registeration Succesful");
+        window.location.href = "../html/home.html"
+    } else {
+        let error = await response.text();
+        alert(error);
+    }
+}
+async function Login1() {
+    let email =
+        document.getElementById("LoginEmail").value;
+    let password =
+        document.getElementById("LoginPassword").value;
+
+    let response = await fetch(
+        "https://localhost:7144/api/User/login",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type":
+                    "application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        }
+
+
+    );
+    
+    if (response.ok) {
+        
+        let result = await response.json();
+        
+        localStorage.setItem("userId",result.id)
+        localStorage.setItem("username", result.name);
+        localStorage.setItem("loggedIn", "true");
+        localStorage.setItem("email", result.email);
+        console.log("Saved email:", localStorage.getItem("email"));
+        alert("Welcome " + result.name);
+        window.location.href = "../html/home.html";
+    } else {
+        let error = await response.text();
+        alert(error);
+    }
+}
+let register =
+    document.querySelector(".register");
+
+let login =
+    document.querySelector(".login");
+
+function openLogin() {
+
+    register.style.width = "15%";
+    login.style.width = "85%";
+    register.style.backgroundImage =  "none";
+    login.style.backgroundImage = "url('../images/background.png')";
+
+    document.querySelector(".login-preview").style.display = "none";
+    document.querySelector(".login-form").style.display = "flex";
+    document.querySelector(".register-afterview").style.display = "flex";
+    document.querySelector(".register-form").style.display = "none";
+}
+
+function openRegister() {
+
+    register.style.width = "85%";
+    login.style.width = "15%";
+    register.style.backgroundImage = "url('../images/background.png')";
+    login.style.backgroundImage = "none";
+
+    document.querySelector(".register-afterview").style.display = "none";
+    document.querySelector(".register-form").style.display = "flex";
+    document.querySelector(".login-preview").style.display = "flex";
+
+    document.querySelector(".login-form").style.display = "none";
+
+}
+
+function ShowPassword2() {
+    let password =
+        document.getElementById("Password");
+    let confirmPassword =
+        document.getElementById("Confirm_Password");
+
+    if (password.type === "password") {
+        password.type = "text";
+        confirmPassword.type = "text";
+    } else {
+        password.type = "password";
+        confirmPassword.type = "password";
+    }
+}
+
+function ShowPassword1() {
+    let password =
+        document.getElementById("LoginPassword");
+
+    if (password.type === "password") {
+        password.type = "text";
+    }
+    else {
+        password.type = "password";
+    }
+}
---
+++ website3/javascript/home.js
@@ -0,0 +1,100 @@
+﻿function open_menu() {
+    document.querySelector(".op_menu").style.display = "flex";
+}
+
+function close_menu() {
+    document.querySelector(".op_menu").style.display = "none";
+}
+
+document.addEventListener("click", function (event) {
+    let menu = document.querySelector(".op_menu");
+    let hamburger = document.querySelector(".hamburger-menu");
+
+    if (!menu.contains(event.target) &&
+        !hamburger.contains(event.target)) {
+        menu.style.display = "none";
+        }
+});
+
+function logout() {
+    localStorage.removeItem("loggedIn");
+    window.location.href = "../html/loginpage.html";
+
+}
+if (localStorage.getItem("loggedIn") !== "true") {
+    window.location.href="../html/login.html"
+}
+
+let username = localStorage.getItem("username");
+document.getElementById("welcomeText").innerText = `Welcome,${username}👋`; 
+
+function open_pd() {
+    window.location.href="../html/Personal_details.html";
+}
+
+function open_m() {
+    window.location.href = "../html/membership.html";
+}
+
+function open_a() {
+    window.location.href = "../html/attendance.html";
+}
+
+function open_mq() {
+    window.location.href = "../html/myQr.html";
+}
+
+function open_c() {
+    window.location.href = "../html/contact.html";
+}
+
+async function loadMembership() {
+    let email = localStorage.getItem("email");
+    let response = await fetch(
+        `https://localhost:7144/api/User/${email}`
+    );
+
+    let user = await response.json();
+
+    document.getElementById("plan").innerText =
+        user.membershipPlan ?? "None";
+
+    document.getElementById("status").innerText =
+        user.membershipStatus ?? "Inactive";
+
+    
+    if (!user.membershipExpiryDate) {
+        document.getElementById("expiresIn").innerText = "Not Available"
+    }
+    else {
+        let expiryDate = new Date(user.membershipExpiryDate);
+
+        let today = new Date();
+
+        let diff = expiryDate - today;
+
+        let daysLeft = Math.ceil(diff / (1000 * 60 * 60 * 24));
+
+
+        if (daysLeft > 0) {
+            document.getElementById("expiresIn").innerText = daysLeft + "Days";
+        }
+        else {
+            document.getElementById("expiresIn").innerText = "Expired";
+        }
+    }
+
+    let userId = localStorage.getItem("userId");
+
+    let attendanceResponse = await fetch(
+        `https://localhost:7144/api/Attendance/user/${userId}`
+    );
+
+    if (!attendanceResponse.ok) return;
+
+    let attendance = await attendanceResponse.json();
+
+    document.getElementById("attendance").innerText = attendance.length;
+}
+
+loadMembership();
